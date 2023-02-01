b162978569f6c91164533ace5fc2458c5285c17e
Anurag Bakode <anuragb@trainee.nrifintech.com>
2023-02-01 16:50:22 +0530

    Insert row by user input is done

--- src/domain/TestInsert.java
+++ src/domain/TestInsert.java
@@ -4,23 +4,52 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.ResultSet;
-
 
 public class TestInsert {
     public static void main(String[] args) {
         String urlMySQL = "jdbc:mysql://localhost:3306/hr";
         String username = "root";
         String password = "12345";
+        Scanner sc = new Scanner(System.in);
 
         try {
             Connection conSQL = DriverManager.getConnection(urlMySQL, username, password);
             System.out.println("Connection Successful");
-            String query = "insert into student( name , age ,address, course) values( 'Kluzner', 50 , 'Africa' , 3003);";
-            Statement stmt = conSQL.createStatement();
-            int x = stmt.executeUpdate(query);
-            System.out.println(x + "record insered succesfully");
-            
+
+            System.out.println("Insert a new Row in the Table Student ------");
+
+            int i = 0;
+            boolean checked = true;
+
+            while (i < 5 && checked) {
+                System.out.println("Enter the name of the Student");
+                String fullName = sc.nextLine();
+                System.out.println("Enter the age of the Student");
+                int stuAge = Integer.parseInt(sc.nextLine());
+                System.out.println("Enter the Address of the Student");
+                String stuAddress = sc.nextLine();
+                System.out.println("Enter the course the Student want's to enroll in");
+                int courseEnrolled = Integer.parseInt(sc.nextLine());
+
+                String query = "insert into student( name , age ,address, course) values(\'" + fullName + "\'," + stuAge +",\'" + stuAddress+ "\'," + courseEnrolled+ ");"; 
+                System.out.println(query);
+                Statement stmt = conSQL.createStatement();
+                int x = stmt.executeUpdate(query);
+                System.out.println(x + "record insered succesfully");
+
+                String response = "";
+                System.out.println("Do you want to Insert Another Row");
+                response = sc.nextLine();
+
+                if(response.equals("no")){
+                    checked = false;
+                }else continue;
+
+                i++;
+            }
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
